a1bfbb40597fb957bcb25850b3fd109202345943
tanerijun <tanerivince@gmail.com>
2022-10-07 23:15:54 +0800

    Add a script to generate post

--- scripts/generatePostTemplate.js
+++ scripts/generatePostTemplate.js
@@ -1,9 +1,35 @@
 const fs = require("fs");
+const slugger = require("github-slugger").slug;
+
+const BASE_URL = "src/contents/posts";
 const postTitle = process.argv[2];
+const postSlug = slugger(postTitle);
+
+const frontmatter = {
+  author: "Vincent Taneri",
+  datetime: new Date().toISOString(),
+  title: postTitle,
+  slug: postSlug,
+  featured: "false",
+  draft: "false",
+  tags: "",
+  ogImage: "",
+  description: "",
+};
 
 console.log("Generating a new post template ...");
 
-const writeStream = fs.createWriteStream("");
-writeStream.write("");
-writeStream.write("");
+fs.mkdirSync(`${BASE_URL}/${postSlug}/`);
+
+const writeStream = fs.createWriteStream(`${BASE_URL}/${postSlug}/index.md`);
+
+writeStream.write("---\n");
+for (const [key, value] of Object.entries(frontmatter)) {
+  writeStream.write(`${key}: ${value}\n`);
+}
+writeStream.write("---\n");
+writeStream.write("\n");
+writeStream.write("<!-- Introductions -->\n");
+writeStream.write("\n");
+writeStream.write("## Table Of Contents");
 writeStream.end();
