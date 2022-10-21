d5d49b723ebc1bc80db9bd46e7661b63960ba861
tanerijun <tanerivince@gmail.com>
2022-10-21 11:23:50 +0800

    Fix issue with remarkPlugins in mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,6 +38,7 @@ export default defineConfig({
     react(),
     sitemap(),
     mdx({
+      remarkPlugins: [remarkGemoji, remarkToc],
       rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]],
     }),
   ],
